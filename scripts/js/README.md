862b3b9d0fd718e835c8c6fefd75f8c9613b8d7b
Mostafa Hassan <mostafa813765@gmail.com>
2023-05-19 23:20:11 +0300

    update home v1 hero sction change slides action

--- scripts/js/README.md
+++ scripts/js/README.md
@@ -5,3 +5,6 @@
 
         // Products class depends Product class
         script(src="scripts/js/classes/products.js", defer)
+
+        // Home v1, v2, and v3 products depend on product js and products js
+        script(src="scripts/js/home.js", defer)
--- scripts/js/slides.js
+++ scripts/js/slides.js
@@ -1,269 +1,66 @@
-// // ==((This script contains Slides for))==
-// // - Hero section slides for Home v1, Home v2, Home v3
-// // - Offers section slides for Home v1
+/**
+ * @file Manages slides.
+ * @author Mostafa Neamatalla <mostafaneamatalla94@gmail.com>
+ */
 
-// // *****************************
-// // **** Hero section slides ****
-// // *****************************
+const changeSliderPrv = () => {
+  if (currentSlide > 0) {
+    // Remove previous active
+    sliderSlides[currentSlide].classList.remove("slide--active");
+    sliderBullets[currentSlide].classList.remove("bullet--active");
 
-// // Controllers selector
-// const heroSlideLeftControler = document.getElementById(
-//   "heroSlideLeftControler"
-// );
-// const heroSlideRightControler = document.getElementById(
-//   "heroSlideRightControler"
-// );
+    currentSlide--;
 
-// // Bullet Selectors
-// const heroSlidesBullet = document.querySelectorAll(
-//   ".hero-section .slides .bullets .bullet"
-// );
+    //   Active new one
+    sliderSlides[currentSlide].classList.add("slide--active");
+    sliderBullets[currentSlide].classList.add("bullet--active");
+  }
+};
 
-// // Slides Selectors
-// const heroSlides = document.querySelectorAll(
-//   ".hero-section .slides .slides__slide"
-// );
+const changeSliderNxt = () => {
+  if (currentSlide < sliderSlides.length - 1) {
+    // Remove previous active
+    sliderSlides[currentSlide].classList.remove("slide--active");
+    sliderBullets[currentSlide].classList.remove("bullet--active");
 
-// // current displaied slide
-// let heroCurrentSlide = 1;
+    currentSlide++;
 
-// // Change Bullet
-// const changeHeroBullet = (current) => {
-//   // Remove active bullet
-//   heroSlidesBullet.forEach((bullet) =>
-//     bullet.classList.remove("bullet--active")
-//   );
+    //   Active new one //   Active new one
+    sliderSlides[currentSlide].classList.add("slide--active");
+    sliderBullets[currentSlide].classList.add("bullet--active");
+  }
+};
 
-//   // Active current bullet
-//   heroSlidesBullet[heroCurrentSlide].classList.add("bullet--active");
-// };
+// Slider slides
+const sliderSlides = document.querySelectorAll("#sliderSlides .slide");
 
-// // Change Slide
-// const changeHeroSlide = (current) => {
-//   // Remove active slide
-//   heroSlides.forEach((slide) =>
-//     slide.classList.remove("slides__slide--active")
-//   );
+// Slidetr controlers
+const sliderControlers = document.querySelectorAll("#sliderControlers .action");
 
-//   // Active current bullet
-//   heroSlides[heroCurrentSlide].classList.add("slides__slide--active");
-// };
+// Slider Bullets
+const sliderBullets = document.querySelectorAll("#sliderBullets .bullet");
 
-// // Click at controlers
-// heroSlideLeftControler.addEventListener("click", () => {
-//   if (heroCurrentSlide <= 0) heroCurrentSlide = 2;
-//   else heroCurrentSlide--;
+let currentSlide = 1;
 
-//   // Change Bullet
-//   changeHeroBullet(heroCurrentSlide);
+// Controlers click
+sliderControlers.forEach((controler) => {
+  controler.addEventListener("click", (e) => {
+    if (e.target.dataset.btn === "previous") changeSliderPrv();
+    if (e.target.dataset.btn === "next") changeSliderNxt();
+  });
+});
 
-//   // Change Slide
-//   changeHeroSlide(heroCurrentSlide);
-// });
+// Slider click
+sliderBullets.forEach((bullet) => {
+  bullet.addEventListener("click", (e) => {
+    // Remove previous active
+    sliderSlides[currentSlide].classList.remove("slide--active");
+    sliderBullets[currentSlide].classList.remove("bullet--active");
 
-// heroSlideRightControler.addEventListener("click", () => {
-//   if (heroCurrentSlide >= 2) heroCurrentSlide = 0;
-//   else heroCurrentSlide++;
+    currentSlide = parseInt(e.target.dataset.bullet) - 1;
 
-//   // Change Bullet
-//   changeHeroBullet(heroCurrentSlide);
-
-//   // Change Slide
-//   changeHeroSlide(heroCurrentSlide);
-// });
-
-// // Change Bullet by click on it
-// heroSlidesBullet.forEach((bullet) =>
-//   bullet.addEventListener("click", (e) => {
-//     if (heroCurrentSlide === parseInt(e.target.dataset.id)) return;
-
-//     heroCurrentSlide = parseInt(e.target.dataset.id);
-
-//     // Change Bullet
-//     changeHeroBullet(heroCurrentSlide);
-
-//     // Change Slide
-//     changeHeroSlide(heroCurrentSlide);
-//   })
-// );
-
-// // Change slides by swipe for touch devices
-// let touchstartX = 0;
-// let touchendX = 0;
-
-// const checkDirection = () => {
-//   if (touchendX < touchstartX) {
-//     if (heroCurrentSlide >= 2) heroCurrentSlide = 0;
-//     else heroCurrentSlide++;
-//   }
-//   if (touchendX > touchstartX) {
-//     if (heroCurrentSlide <= 0) heroCurrentSlide = 2;
-//     else heroCurrentSlide--;
-//   }
-
-//   // Change Bullet
-//   changeHeroBullet(heroCurrentSlide);
-
-//   // Change Slide
-//   changeHeroSlide(heroCurrentSlide);
-// };
-
-// document.addEventListener("touchstart", (e) => {
-//   touchstartX = e.changedTouches[0].screenX;
-// });
-
-// document.addEventListener("touchend", (e) => {
-//   touchendX = e.changedTouches[0].screenX;
-//   checkDirection();
-// });
-
-// // *******************************
-// // **** Offers section slides ****
-// // *******************************
-
-// // Bullet Selectors
-// const offersSlidesBullet = document.querySelectorAll(
-//   ".offers-section .offers-section__slides .bullets .bullet"
-// );
-
-// // Slides Selectors
-// const offersSlides = document.querySelectorAll(
-//   ".offers-section .offers-section__slides .slide"
-// );
-
-// let offersCurrentSlide = 2;
-
-// // change slide
-// const changeOffersSlide = (slideNum) => {
-//   offersCurrentSlide = slideNum;
-
-//   // remove last active bullet
-//   offersSlidesBullet.forEach((bullet) =>
-//     bullet.classList.remove("bullet--active")
-//   );
-
-//   // active current active bullet
-//   offersSlidesBullet[offersCurrentSlide].classList.add("bullet--active");
-
-//   // remove last active slide
-//   offersSlides.forEach((slide) => slide.classList.remove("slide--active"));
-
-//   // add current active slide
-//   offersSlides[offersCurrentSlide].classList.add("slide--active");
-// };
-
-// // Change slides by clicking bullets
-// offersSlidesBullet.forEach((bullet) =>
-//   bullet.addEventListener("click", (e) =>
-//     changeOffersSlide(parseInt(e.target.dataset.id))
-//   )
-// );
-
-// // *******************************
-// // ****** Tab Slides Cards *******
-// // *******************************
-
-// // Tab Slides Sellector
-// const slideCards = document.querySelector(".tab-slides .slide-cards");
-
-// // cotrollers sellector
-// const slideCardsLeft = document.querySelector(".tab-slides #leftControler");
-// const slideCardsRight = document.querySelector(".tab-slides #rightControler");
-
-// const cardWidth = 300;
-
-// slideCardsLeft.addEventListener("click", () => {
-//   slideCards.scrollLeft += cardWidth;
-// });
-
-// slideCardsRight.addEventListener("click", () => {
-//   slideCards.scrollLeft -= cardWidth;
-// });
-
-// // *******************************
-// // ***** From Blogs Slides *******
-// // *******************************
-// const blogsSlidesPosts = document.querySelector(
-//   ".from-blogs-section .slides .posts"
-// );
-// const blogsSlidesPostsLeft = document.querySelector(
-//   ".from-blogs-section .slides #leftControler"
-// );
-// const blogsSlidesPostsRight = document.querySelector(
-//   ".from-blogs-section .slides #rightControler"
-// );
-
-// const monthShortNames = [
-//   "Jan",
-//   "Feb",
-//   "Mar",
-//   "Apr",
-//   "May",
-//   "Jun",
-//   "Jul",
-//   "Aug",
-//   "Sep",
-//   "Oct",
-//   "Nov",
-//   "Dec",
-// ];
-
-// // Get posts
-// fetch("http://localhost:3000/posts")
-//   .then((res) => res.json())
-//   .then(async (posts) => {
-//     const postsLength = posts.length;
-//     let postsHTML = "";
-
-//     for (let i = postsLength - 1; i >= postsLength - 5; i--) {
-//       let { id, title, short_description, date, image } = posts[i];
-//       let postCommentsLength = 0;
-
-//       // Date handle
-//       let monthFromDate = monthShortNames[new Date(date).getMonth()];
-//       let dayFromDate = new Date(date).getDate();
-//       let yearFromDate = new Date(date).getFullYear();
-
-//       // Fetch comments
-//       await fetch("http://localhost:3000/comments")
-//         .then((res) => res.json())
-//         .then((comments) => {
-//           comments.forEach((comment) => {
-//             if (comment.postid == id) postCommentsLength++;
-//           });
-//         });
-
-//       postsHTML += `
-//       <div class="blog-post-review grid">
-//         <div class="img-container blog-post-review__img">
-//           <img src="${image}" alt="${title}"/>
-//         </div>
-//         <div class="blog-post-review__content">
-//           <h3 class="title">${title}</h3>
-//           <p class="preview">${short_description}</p>
-//           <div class="details">
-//             <div class="details__data"><i class="fa-regular fa-calendar-days"></i><span>${monthFromDate}  ${dayFromDate}, ${yearFromDate}</span></div>
-//             <div class="details__data"><i class="fa-light fa-comment"></i><span>${postCommentsLength}</span></div>
-//             <div class="details__data read-more"><i class="fa-solid fa-caret-right"></i><a href="#">Read More</a></div>
-//           </div>
-//         </div>
-//       </div>`;
-//     }
-
-//     blogsSlidesPosts.innerHTML = postsHTML;
-//   });
-
-// let scrollPostRange = 596;
-
-// // Change slides
-// blogsSlidesPostsRight.addEventListener("click", () => {
-//   blogsSlidesPosts.scrollLeft += scrollPostRange;
-// });
-
-// blogsSlidesPostsLeft.addEventListener("click", () => {
-//   if (blogsSlidesPosts.scrollLeft <= scrollPostRange + scrollPostRange / 2) {
-//     blogsSlidesPosts.scrollLeft = 0;
-//   } else {
-//     blogsSlidesPosts.scrollLeft -= scrollPostRange;
-//   }
-// });
+    //   Active new one
+    sliderSlides[currentSlide].classList.add("slide--active");
+    sliderBullets[currentSlide].classList.add("bullet--active");
+  });
+});
