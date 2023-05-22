1295d63f21ea96c1bc86b24752ea36bb02e68f9f
Mostafa Hassan <mostafa813765@gmail.com>
2023-05-23 00:28:51 +0300

    update render featured products at home v3

--- scripts/js/home-v3.js
+++ scripts/js/home-v3.js
@@ -112,3 +112,73 @@ populerCategories.forEach(
     </a>
     `)
 );
+
+// Home new products
+const newProducts = document.getElementById("newProducts");
+
+// Home top products
+const topProducts = document.getElementById("topProducts");
+
+// Home top products tabs
+const topProductsTabs = document.querySelectorAll(
+  "#topProductsTabs .tabs-changer__tab"
+);
+
+/**
+ * Fetching array of data from server ("/proucts")
+ * @param {Array} data
+ */
+const handleData = (data) => {
+  const products = new Products(data, "grid");
+
+  products.customRender("all-featured");
+  topProducts.innerHTML = products.productsRender();
+
+  // Change displayed products pased on tab click.
+  topProductsTabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+      topProductsTabs.forEach((item) =>
+        item.classList.remove("tabs-changer__tab--active")
+      );
+
+      if (e.target.dataset.tab === "all-featured") {
+        tab.classList.add("tabs-changer__tab--active");
+        products.customRender("all-featured");
+      }
+
+      if (e.target.dataset.tab === "shoes-featured") {
+        tab.classList.add("tabs-changer__tab--active");
+        products.customRender("shoes-featured");
+      }
+
+      if (e.target.dataset.tab === "watches-featured") {
+        tab.classList.add("tabs-changer__tab--active");
+        products.customRender("watches-featured");
+      }
+
+      topProducts.innerHTML = products.productsRender();
+    });
+  });
+};
+
+/**
+ * Handle data from server
+ * @param {*} res
+ * @returns error or data
+ */
+const handleServerError = (res) => {
+  if (!res.ok) console.log("Server has somthing worng");
+  else return res.json();
+};
+
+/**
+ * Handle errors while fetching data.
+ * @param {Object} error
+ */
+const handleDataError = (error) => {
+  console.log(error);
+};
+
+fetch("http://localhost:3000/products")
+  .then(handleServerError)
+  .then(handleData);
